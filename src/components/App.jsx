023d39a03e36a883baf112d7d2fda5a7c78b3dba
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-09-07 13:19:33 +0300

    Api, App, Searchbar base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,19 @@
 import { Component } from 'react';
 import React from 'react';
-import {Searchbar} from './Searchbar/Searchbar'
+import { Searchbar } from './Searchbar/Searchbar'
+import {fetchImages} from './Api'
+// import axios from 'axios';
+import { Audio } from 'react-loader-spinner'
+;<Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+
 
 
 
@@ -9,25 +22,61 @@ export class App extends Component{
     query: '',
     images: [],
     page: 1,
+    per_page: 12,
+    loadMore: false,
+    error: null,
     };
   
-      handleSubmit = evt => {
-    evt.preventDefault();
-    this.setState({
-      query: `${Date.now()}/${evt.target.elements.query.value}`,
-      images: [],
-      page: 1,
-    });
-      };
   
+componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages(query, page);
+    }
+  }
+
+  getImages = async (query, page) => {
+    if (!query) {
+      return;
+    }
+    try {
+      const { hits, totalHits } = await fetchImages(query, page);
+      if (!hits.length) {
+        this.setState({ loadMore: false });
+        alert('Nothing was found for your request. Try something else');
+      } else {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...hits],
+          loadMore: this.state.page < Math.ceil(totalHits / this.state.per_page),
+        }));
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+    handleSubmit = (query) => {
+      this.setState({ query, images: [], page: 1 });
+    };
   
+
+  onloadMore = () => {
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
+    this.scrollToBottomButton();
+  };
+
 
 
 
   render() {
+
+        // const { images, loadMore } = this.state;
+
       return (
         <div>
-          <Searchbar/>
+          <Searchbar handleSubmit={this.handleSubmit} />
     </div>
   );
   }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,16 +1,38 @@
-export const Searchbar = () => {
+import { Component } from 'react';
+
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ query: event.currentTarget.value.toLowerCase()});
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+        this.props.handleSubmit(this.state.query);
+        this.setState({ query: '' });
+    };
+  
+
+  render() {
     return (
-      <form >
-    <button type="submit">
-      <span >Search</span>
-    </button>
-    <input
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
+      <header className="searchbar">
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+          <input
+            type="text"
+            id="search"
+            value={this.state.query}
+            onChange={this.handleNameChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos" />
+        </form>
+      </header>
     );
-    
+  }
 }
